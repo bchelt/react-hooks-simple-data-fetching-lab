7f92d1a55e90272cd266332e99b7cdd08634dd87
Brandon Helt <brandon.helt@yahoo.com>
2022-06-18 16:08:55 -0700

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dog, setDog] = useState('');
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setDog(data.message)
+            setLoaded(true);
+        })
+    }, []);
+    if (!loaded) return <p>Loading...</p>;
+    return (
+        <img src={dog} alt={'A Random Dog'}/>
+    )
+}
+
+export default App;
